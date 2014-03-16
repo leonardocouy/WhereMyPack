d53a4f22634c4296a22dcdafcadb1f3449a76eef
Leonardo Couy <leonardocouy@hotmail.com>
2014-03-16 10:48:46 -0300

    * Adicionado o envio automatico de email, caso a encomenda foi cadastrada com sucesso no datastore
    * Caso a encomenda esteja com o mesmo status, ele retorna que não houve atualizações ainda.
    
    * Encomenda por usuario do google.
    
    * Listar as encomendas cadastradas pelo usuario com AngularJS
    
    * Bugs arrumados

--- backend/src/web/home.py
+++ backend/src/web/home.py
@@ -5,7 +5,6 @@ from google.appengine.api import users
 
 def index(_write_tmpl):
     usuario = users.get_current_user()
-    # dict_status = {'usuario': usuario}
     if usuario:
         _write_tmpl('templates/base.html')
     else:
--- backend/src/web/painel.py
+++ backend/src/web/painel.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from google.appengine.ext import ndb
-from google.appengine.api import users
+from google.appengine.ext import ndb, deferred
+from google.appengine.api import users, taskqueue
 from lib.correio import correio
 from tekton import router
-from datetime import datetime
+from web.autentifer import usuariogoogle
+
+
+user_google = users.get_current_user()
+dict_google = {'user_google': user_google.nickname()}
 
 
 class Pacote(ndb.Model):
     cod_rastreio = ndb.StringProperty()
-    id_google = ndb.StringProperty()
     nome_google = ndb.StringProperty()
     email = ndb.StringProperty()
     local = ndb.StringProperty()
@@ -20,40 +23,102 @@ class Pacote(ndb.Model):
 
 def extrai_infos(cod_rastreio):
     source_html = correio.captura_html(
-        'http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI=%s'%cod_rastreio)
+        'http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI=%s' % cod_rastreio)
     status = correio.extracao(source_html)
     return status
 
 
-def salvar(_write_tmpl, _resp, cod_rastreio, email):
-    user_google = users.get_current_user()
+def verificar_status():
+    allregisters = Pacote.query()
+    for reg_sel in allregisters.fetch():
+        infos = reg_sel.to_dict()
+        correio.destinatario = infos['email']
+        correio.cod_rastreio = infos['cod_rastreio']
+        status = extrai_infos(infos['cod_rastreio'])
+
+        pacote = Pacote(cod_rastreio=infos['cod_rastreio'],
+                        email=infos['email'],
+                        local=status['local'], encaminhado=status['encaminhado'], situacao=status['situacao'],
+                        data=status['data'], nome_google=infos['nome_google'])
+
+        query = Pacote.query().filter(Pacote.data == status['data']).filter(
+            Pacote.nome_google == users.get_current_user().nickname()).filter(
+            Pacote.cod_rastreio == status['codigorast'])
+        datadb = {'data': '00/00/0000', 'situacao': 'nada'}
+
+        for c in query.fetch():
+            datadb = c.to_dict()
+
+        if datadb['data'] != status['data']:
+            pacote.put()
+            correio.email(status, reg_sel.email)
+
+
+def salvar(_write_tmpl, cod_rastreio, email):
     correio.destinatario = email
     correio.cod_rastreio = cod_rastreio
-
     status = extrai_infos(cod_rastreio)
-    query = Pacote.query().filter(Pacote.data == status ['data'] and Pacote.cod_rastreio == status ['codigorast'])
-    pacote = Pacote(cod_rastreio=cod_rastreio, id_google=user_google.user_id(), nome_google=user_google.nickname(),
-                    email=email,
-                    local=status['local'], encaminhado=status['encaminhado'], situacao=status['situacao'],
-                    data=status['data'])
-    datadb = {'data': '00/00/0000'}
-    for c in query.fetch():
-        datadb = c.to_dict()
-
-
-    if datadb['data'] != status['data']:
-        pacote.put()
-        correio.email(status, email)
-        _resp.write("Envio sucesso!")
+    if status == {}:
+        errodict = {'erro': 'Este código não existe, ou não possui registros ainda.'}
+        _write_tmpl('/templates/painel_erros.html', errodict)
     else:
-        _resp.write("Não houve nenhuma alteração no seu rastreio ainda...")
-    envio_dict = {"status_envio": correio.message_error}
-    #_write_tmpl('templates/painel_index.html', envio_dict)
+        query = Pacote.query().filter(Pacote.data == status['data']).filter(
+        Pacote.nome_google == users.get_current_user().nickname()).filter(Pacote.cod_rastreio == status['codigorast'])
+        pacote = Pacote(cod_rastreio=cod_rastreio,
+                        email=email,
+                        local=status['local'], encaminhado=status['encaminhado'], situacao=status['situacao'],
+                        data=status['data'], nome_google=usuariogoogle.nickname())
+        datadb = {'data': '00/00/0000'}
+        for c in query.fetch():
+            datadb = c.to_dict()
+
+        if datadb['data'] != status['data']:
+            pacote.put()
+            correio.email(status, email)
+            errodict = {'erro': 'Código cadastrado com sucesso no sistema'}
+            _write_tmpl('/templates/painel_erros.html', errodict)
+        else:
+            errodict = {'erro': 'Este código já esta cadastrado no seu usuario.'}
+            _write_tmpl('/templates/painel_erros.html', errodict)
+
 
 def cad_rastreio(_write_tmpl):
     path = router.to_path(salvar)
     dct = {'salvar_rastreio': path}
     _write_tmpl('/templates/painel_cadastrar.html', dct)
+
+
+def listar_codigos(_json):
+    query = Pacote.query().filter(Pacote.nome_google == users.get_current_user().nickname())
+    listinha = []
+    for x in query.fetch():
+        datadb = x.to_dict()
+        listinha.append(datadb['cod_rastreio'])
+    listinha2 = list(set(listinha))
+    _json(listinha2, '')
+
+
+def listar_rastreio(_json, _write_tmpl, cod_rastreio):
+    query = Pacote.query().filter(Pacote.nome_google == usuariogoogle.nickname()).filter(
+        Pacote.cod_rastreio == cod_rastreio)
+    query = query.order(-Pacote.data)
+    encomendas = query.fetch()
+    encomendas_dct = [p.to_dict() for p in encomendas]
+    if encomendas == []:
+        errodict = {'erro': 'Você não possui encomenda cadastrada!'}
+        _write_tmpl('/templates/painel_erros.html', errodict)
+    else:
+        _json(encomendas_dct, '')
+
+
+def listar(_write_tmpl):
+    query = Pacote.query().filter(Pacote.nome_google == usuariogoogle.nickname())
+    verificar_encomendas = query.fetch()
+    if verificar_encomendas == []:
+        errodict = {'erro': 'Você não possui encomenda cadastrada!'}
+        _write_tmpl('/templates/painel_erros.html', errodict)
+    else:
+        _write_tmpl('/templates/painel_listar.html')
 
 
 def index(_write_tmpl):
