c804741b6d9b695281554d1f298e6bf16816dac7
Leonardo Couy <leonardocouy@hotmail.com>
2014-03-16 20:00:40 -0300

    * Adicionado o envio automatico de email, caso a encomenda foi cadastrada com sucesso no datastore
    * Caso a encomenda esteja com o mesmo status, ele retorna que não houve atualizações ainda.
    
    * Encomenda por usuario do google.
    
    * Listar as encomendas cadastradas pelo usuario com AngularJS
    
    * Bugs arrumados
    
    * Usuario online no menu.

--- backend/src/web/autenticar.py
+++ backend/src/web/autenticar.py
@@ -3,9 +3,14 @@ from __future__ import absolute_import, unicode_literals
 from google.appengine.api import users
 from tekton import router
 
+usuario = users.get_current_user()
+
+def user(_json):
+    nome_google = {'nome' : users.get_current_user().nickname()}
+    _json(nome_google, '')
 
 def index(_handler, _req):
-    usuario = users.get_current_user()
+
     if usuario:
        # url = router.to_path('/')
         _handler.redirect('/')
